b3d6a9ee7e692a37b5087e66160a0b3dc8252966
Foxy <kseniya250496@mail.ru>
2022-11-17 21:53:20 +0300

    changes for task 7

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -6,6 +6,12 @@
 
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
-int lastNum = number % 10;
-Console.WriteLine($"Последняя цифра числа: {lastNum}");
+if (number > 99 & number < 1000)
+{
+    int lastNum = number % 10;
+    Console.WriteLine($"Последняя цифра числа: {lastNum}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Число не трехзначное");
+}
